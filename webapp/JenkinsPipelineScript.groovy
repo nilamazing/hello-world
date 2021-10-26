938f3920dfa434917cfd83e55b04f2383cebc247
Nilanjan Dutta <duttnilanjan@gmail.com>
2021-10-26 17:43:36 +0100

    Temporary substituted maven job with shell script

--- webapp/JenkinsPipelineScript.groovy
+++ webapp/JenkinsPipelineScript.groovy
@@ -7,10 +7,12 @@ job('First_Jenkins_Project_Via_DSL'){
         scm('* * * * *')
     }
     steps{
-        maven{
-            mavenInstallation("MAVEN_HOME")
-        }
-        maven('clean package', 'webapp/pom.xml')
+        // maven{
+        //     mavenInstallation("MAVEN_HOME")
+        // }
+        // maven('clean package', 'webapp/pom.xml')
+        shell('echo Building webapp package')
+        shell('/opt/maven/bin/mvn -f webapp/pom.xml clean package')
     }
     publishers{
         archiveArtifacts{
