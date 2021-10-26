9eab2928404ee493d0f0900697f073954476d77b
Nilanjan Dutta <duttnilanjan@gmail.com>
2021-10-26 17:13:33 +0100

    Added Pipeline Groovy Script

---
+++ webapp/JenkinsPipelineScript.groovy
@@ -0,0 +1,18 @@
+job('First_Jenkins_Project_Via_DSL'){
+    description('First Jenkins Project Via DSL')
+    scm{
+        git('https://github.com/nilamazing/hello-world.git','master')
+    }
+    triggers{
+        scm('* * * * *')
+    }
+    steps{
+        maven('clean package', 'webapp/pom.xml')
+    }
+    publishers{
+        archiveArtifacts{
+            pattern('**/*.war')
+            onlyIfSuccessful()
+        }
+    }
+}
